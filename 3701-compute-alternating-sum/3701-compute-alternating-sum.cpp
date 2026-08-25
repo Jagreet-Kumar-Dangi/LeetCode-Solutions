50836ca57940e665e7f3b5e72d362c2f686fe188
Jagreet Kumar Dangi <jagreetdangi2007@gmail.com>
2026-08-25 19:55:21 +0530

    Time: 0 ms (100%), Space: 32.5 MB (78.71%) - LeetHub

--- 3701-compute-alternating-sum/3701-compute-alternating-sum.cpp
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.cpp
@@ -3,11 +3,14 @@ public:
     int alternatingSum(vector<int>& nums) {
         int sum=0;
         int n=nums.size();
+        int sign=1;
         for(int i=0;i<n;i++){
-            if(i%2==0)
-                sum +=nums[i];
-            else
-                sum -=nums[i];
+            // if(i%2==0)
+            //     sum +=nums[i];
+            // else
+            //     sum -=nums[i];
+            sum +=sign*nums[i];
+            sign = -sign;
         }
         return sum;
     }
