79688011793fe5a24a7c7069558e15abc7fe8ef7
Jagreet Kumar Dangi <jagreetdangi2007@gmail.com>
2026-08-29 15:42:10 +0530

    Time: 3 ms (43.77%), Space: 8.3 MB (51.47%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string interpret(string command) {
+        string s="";
+        for(int i=0;i<command.size();i++){
+            if(command[i]=='G')
+                s +='G';
+            else if(command[i]=='(' && command[i+1] !='a')
+                s+='o';
+            else if(command[i]=='(' && command[i+1] =='a')
+                s+="al";
+        }
+        return s;
+    }
+};
